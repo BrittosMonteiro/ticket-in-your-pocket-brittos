9cdbfd6566281d9dcf77456e8cb8abcb1d4b710e
Lucas Brittos <brittosmonteiro@gmail.com>
2024-03-25 19:58:45 -0300

    Início do header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import HeaderComponent from './components/Header';
 
@@ -8,9 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <HeaderComponent />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
+import './style.css'
 import logo from "../../logo.svg";
 
 export default function HeaderComponent() {
   return (
-    <header>
-      <img src={logo} className="App-logo" alt="logo" />
+    <header className="header">
+      <img src={logo} className="logo" alt="logo" />
+      <input type="text" name="search_movie" className="input-default" placeholder="Procurar filme" />
+      <a href="#">Acessar minha conta</a>
     </header>
   );
 }

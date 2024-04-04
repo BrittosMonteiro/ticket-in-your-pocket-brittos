2f5308d4ac03671acd22df18c2a3d201885910e3
Lucas Brittos <brittosmonteiro@gmail.com>
2024-04-04 20:52:52 -0300

    Altera quantidades

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,6 +4,23 @@ import { Ticket, Minus } from "phosphor-react";
 
 export default function CardComponent({ movie }) {
   const [qty, setQty] = useState(0);
+
+  const addTicket = () => {
+    setQty(qty + 1);
+  };
+
+  const removeTicket = () => {
+    setQty(qty - 1);
+  };
+
+  const changeQty = (newQty) => {
+    if (newQty < 0 || !newQty) {
+      setQty(0);
+    } else {
+      setQty(newQty);
+    }
+  };
+
   return (
     <div className="card">
       <img
@@ -14,19 +31,25 @@ export default function CardComponent({ movie }) {
         <span className="card-title">{movie.title}</span>
         <button
           className="btn-default btn-add-to-cart"
-          onClick={() => setQty(qty + 1)}
+          onClick={() => addTicket()}
         >
           Adicionar
         </button>
         <div className="card-row">
           <button
-            className="btn-default"
-            onClick={() => setQty(qty - 1)}
+            className="btn-default btn-remove-from-cart"
+            onClick={() => removeTicket()}
             disabled={qty == 0}
           >
             <Minus color="#f4f8fc" />
           </button>
-          <input type="text" defaultValue={qty} disabled={qty == 0} />
+          <input
+            type="text"
+            className="input-qty-ticket"
+            value={qty}
+            disabled={qty == 0}
+            onChange={(e) => changeQty(e.target.value)}
+          />
           <Ticket size={18} color="#f4f8fc" />
         </div>
       </div>

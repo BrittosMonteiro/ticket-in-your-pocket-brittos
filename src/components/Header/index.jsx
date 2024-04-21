b4e999ed6b84b0e5de4a4b0de4183ac6961f3a04
Anna-csn <annaclarasnascimento@hotmail.com>
2024-04-21 16:14:32 -0300

    Adicionando botões ao navbar e aplicação de estilos

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,15 +3,21 @@ import "./style.css";
 import { ShoppingCartSimple } from "phosphor-react";
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
+import logo from "../../../src/assets/TicketInYourPocketLogo.svg";
+import Button from "../Button";
 
 export default function HeaderComponent() {
-  const {getCartQty} = useContext(CartContext)
+  const { getCartQty } = useContext(CartContext);
   return (
     <header className="header">
       <Link to={"/"}>
-        <h1 className="title">TicketInYourPocket</h1>
+        <img src={logo} alt="TicketInYourPocket" className="logo" />
       </Link>
-      <Link to={"/cart"}>
+      <div className="menu-right">
+      <Button onClick={() => alert('Botão 1 clicado!')}>Login</Button>
+      <Button onClick={() => alert('Botão 2 clicado!')}>Cadastre-se</Button>
+      </div>
+      <Link to={"/cart"} className="cart-link">
         <ShoppingCartSimple color="#fff" size={28} />
         <sub>{getCartQty()}</sub>
       </Link>
